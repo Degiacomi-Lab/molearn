fbee53703293ca9f54e756687ebc2e2210ff7f05
Samuel Musson <wppj21@ncc1.clients.dur.ac.uk>
2023-03-28 15:06:24 +0100

    get_datasets for sinkhorn loss added to pdbdata

--- src/molearn/pdb_data.py
+++ src/molearn/pdb_data.py
@@ -68,7 +68,7 @@ class PDBData:
             self.mean = self.dataset.mean()
         self.dataset -= self.mean
         self.dataset /= self.std
-        self.dataset = torch.from_numpy(self.dataset)
+        self.dataset = torch.from_numpy(self.dataset).float()
         self.dataset = self.dataset.permute(0,2,1)
         print(f'Dataset.shape: {self.dataset.shape}')
         print(f'mean: {str(self.mean)}, std: {str(self.std)}')
@@ -89,7 +89,7 @@ class PDBData:
         M.properties['center'] = M.get_center()
         return deepcopy(M)
 
-    def get_dataloader(self, batch_size, validation_split=0.1, pin_memory=True, dataset_sample_size=-1, manual_seed=None):
+    def get_dataloader(self, batch_size, validation_split=0.1, pin_memory=True, dataset_sample_size=-1, manual_seed=None,shuffle=True, sampler=None):
         if not hasattr(self, 'dataset'):
             self.prepare_dataset()
         valid_size = int(len(self.dataset)*validation_split)
@@ -97,11 +97,40 @@ class PDBData:
         dataset = torch.utils.data.TensorDataset(self.dataset.float())
         if manual_seed is not None:
             self.train_dataset, self.valid_dataset = torch.utils.data.random_split(dataset, [train_size, valid_size], generator=torch.Generator().manual_seed(manual_seed))
+            self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size, pin_memory=pin_memory,
+                                                                sampler=torch.utils.data.RandomSampler(self.train_dataset,generator=torch.Generator().manual_seed(manual_seed)))
         else:
             self.train_dataset, self.valid_dataset = torch.utils.data.random_split(dataset, [train_size, valid_size])
-        self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size, pin_memory=pin_memory, shuffle=True)
-        self.valid_dataloader = torch.utils.data.DataLoader(self.valid_dataset, batch_size=batch_size, pin_memory=pin_memory)
+            self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset, batch_size=batch_size, pin_memory=pin_memory, shuffle=True)
+        self.valid_dataloader = torch.utils.data.DataLoader(self.valid_dataset, batch_size=batch_size, pin_memory=pin_memory,shuffle=True)
         return self.train_dataloader, self.valid_dataloader
+
+    def get_datasets(self, validation_split=0.1, valid_size=None, train_size=None, manual_seed = None):
+        '''
+            returns torch.Tensor for training and validation structures.
+        '''
+        if not hasattr(self, 'dataset'):
+            self.prepare_dataset()
+        dataset = self.dataset.float()
+        if train_size is None:
+            _valid_size = int(len(self.dataset)*validation_split)
+            _train_size = len(self.dataset) - _valid_size
+        else:
+            _train_size = train_size
+            if valid_size is None:
+                _valid_size = validation_split*_train_size
+            else:
+                _valid_size = valid_size
+        from torch import randperm
+        if manual_seed is not None:
+            indices = randperm(len(self.dataset), generator = torch.Generator().manual_seed(manual_seed))
+        else:
+            indices = randperm(len(self.dataset))
+        train_dataset = dataset[indices[:_train_size]]
+        valid_dataset = dataset[indices[_train_size:_train_size+_valid_size]]
+        return train_dataset, valid_dataset
+        
+
 
     @property
     def atoms(self):
