3774a28254eda7cddf51a780cbe4306585f87d8a
Asal Azar <yabiasal@gmail.com>
2024-01-18 12:56:34 +0000

    use fraction of a dataset

--- examples/analysis_example.py
+++ examples/analysis_example.py
@@ -1,3 +1,5 @@
+#%%
+# Import libraries
 import torch
 import os
 from molearn.models.foldingnet import AutoEncoder
@@ -5,72 +7,122 @@ from molearn.analysis import MolearnAnalysis
 from molearn.data import PDBData
 import matplotlib.pyplot as plt
 
+#%%
+# Specify FUll_DATASETS
+if __name__ =='__main__':
+    FULL_DATASETS = True
+        
+    if not FULL_DATASETS:
+        device = torch.device('cuda')
+        MA_processes = 16
+        #torch.set_num_threads(16)
+    else:
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        MA_processes = 4
 
-print("> Loading network parameters...")
+#%%
+# Load network 
+if __name__ == '__main__':
+    print("> Loading network parameters...")
 
-fname = f'xbb_foldingnet_checkpoints{os.sep}checkpoint_no_optimizer_state_dict_epoch167_loss0.003259085263643.ckpt'
-# change 'cpu' to 'cuda' if you have a suitable cuda enabled device
-checkpoint = torch.load(fname, map_location=torch.device('cpu'))
-net = AutoEncoder(**checkpoint['network_kwargs'])
-net.load_state_dict(checkpoint['model_state_dict'])
+    fname = f'xbb_foldingnet_checkpoints{os.sep}checkpoint_no_optimizer_state_dict_epoch167_loss0.003259085263643.ckpt'
+    # change 'cpu' to 'cuda' if you have a suitable cuda enabled device
+    #checkpoint = torch.load(fname, map_location=torch.device('cpu'))
+    checkpoint = torch.load(fname, map_location=torch.device('cuda'))
+    net = AutoEncoder(**checkpoint['network_kwargs'])
+    net.load_state_dict(checkpoint['model_state_dict'])
 
-print("> Loading training data...")
+#%%
+# Created MA object, import train and valid data
+if __name__ == '__main__':
+    print("> Loading training data...")
 
-MA = MolearnAnalysis()
-MA.set_network(net)
+    MA = MolearnAnalysis()
+    MA.set_network(net)
 
-# increasing the batch size makes encoding/decoding operations faster,
-# but more memory demanding
-MA.batch_size = 4
+    # increasing the batch size makes encoding/decoding operations faster,
+    # but more memory demanding
+    MA.batch_size = 4
 
-# increasing processes makes DOPE and Ramachandran scores calculations faster,
-# but more more memory demanding
-MA.processes = 2
+    # increasing processes makes DOPE and Ramachandran scores calculations faster,
+    # but more more memory demandingTrue
+    MA.processes = 2
 
-# what follows is a method to re-create the training and test set
-# by defining the manual see and loading the dataset in the same order as when
-#the neural network was trained, the same train-test split will be obtained
-data = PDBData()
-#data.import_pdb(f'data{os.sep}MurD_closed_selection.pdb')
-#data.import_pdb(f'data{os.sep}MurD_open_selection.pdb')
-data.import_pdb('/home3/pghw87/trajectories/MurD/MurD_closed.pdb')
-data.import_pdb('/home3/pghw87/trajectories/MurD/MurD_open.pdb')
-data.fix_terminal()
-data.atomselect(atoms = ['CA', 'C', 'N', 'CB', 'O'])
-data.prepare_dataset()
-data_train, data_test = data.split(manual_seed=25)
+    # what follows is a method to re-create the training and test set
+    # by defining the manual see and loading the dataset in the same order as when
+    #the neural network was trained, the same train-test split will be obtained
+    data = PDBData()
+    data.import_pdb(f'data{os.sep}MurD_closed_selection.pdb')
+    data.import_pdb(f'data{os.sep}MurD_open_selection.pdb')
+    #data.import_pdb('/home3/pghw87/trajectories/MurD/MurD_closed.pdb')
+    #data.import_pdb('/home3/pghw87/trajectories/MurD/MurD_open.pdb')
+    data.fix_terminal()
+    data.atomselect(atoms = ['CA', 'C', 'N', 'CB', 'O'])
+    data.prepare_dataset()
+    data_train, data_valid = data.split(manual_seed=25)
+    data_test = PDBData()
+    data_test.import_pdb(f'data{os.sep}MurD_closed_apo_selection.pdb')
+    data_test.std = data.std
+    data_test.mean = data.mean
+    data_test.fix_terminal()
+    data_test.atomselect(atoms = ['CA', 'C', 'N', 'CB', 'O'])
+    data_test.prepare_dataset()
 
-# store the training and test set in the MolearnAnalysis instance
+#%%
+# specify the fraction of data to use for debugging
+# if FULL_DATASET is as default (false), this chunk of code will run 
+if __name__ == '__main__':
+    if not FULL_DATASETS:
+        data_train.dataset = data_train.dataset[::40]
+        data_valid.dataset = data_valid.dataset[::4]
+        data_test.dataset = data_test.dataset[::10]
+
+#%%
+# print out the size of train, valid, test data
+if __name__ == '__main__':
+    print(f'Train set shape {data_train.dataset.shape}, Valid set shape {data_valid.dataset.shape}, Test set shape {data_test.dataset.shape}')
+#%%
+# store the training and valid set in the MolearnAnalysis instance
 # the second parameter of the sollowing commands can be both a PDBData instance
 # or a path to a multi-PDB file
-MA.set_dataset("training", data_train)
-MA.set_dataset("test", data_test)
-
-print("> calculating RMSD of training and test set")
-
-err_train = MA.get_error('training')
-err_test = MA.get_error('test')
-
-print(f'Mean RMSD is {err_train.mean()} for training set and {err_test.mean()} for test set')
-fig, ax = plt.subplots()
-violin = ax.violinplot([err_train, err_test], showmeans = True, )
-ax.set_xticks([1,2])
-ax.set_title('RMSD of training and test set')
-ax.set_xticklabels(['Training', 'Test'])
-plt.savefig('RMSD_plot.png')
+if __name__ == '__main__':
+    MA.set_dataset("training", data_train)
+    MA.set_dataset("valid", data_valid)
+    MA.set_dataset('test', data_test)
 
 
-print("> generating error landscape")
-# build a 50x50 grid. By default, it will be 10% larger than the region occupied
-# by all loaded datasets
-MA.setup_grid(50)
-landscape_err_latent, landscape_err_3d, xaxis, yaxis = MA.scan_error()
+#%%
+#Calculate RMSD of training and test set
+if __name__ == '__main__':
+    print("> calculating RMSD of training and test set")
 
-fig, ax = plt.subplots()
-c = ax.pcolormesh(xaxis, yaxis, landscape_err_latent)
-plt.savefig('Error_grid.png')
+    err_train = MA.get_error('training')
+    err_valid = MA.get_error('valid')
+    err_test = MA.get_error('test')
+
+    print(f'Mean RMSD is {err_train.mean()} for training set and {err_valid.mean()} for valid set and {err_test.mean()} for test set')
+    fig, ax = plt.subplots()
+    violin = ax.violinplot([err_train, err_valid, err_test], showmeans = True, )
+    ax.set_xticks([1,2,3])
+    ax.set_title('RMSD of training and test set')
+    ax.set_xticklabels(['Training', 'Validation','Testing'])
+    plt.savefig('RMSD_plot.png')
+
+ #%%
+#Calculate error
+if __name__ == '__main__':
+    print("> generating error landscape")
+    # build a 50x50 grid. By default, it will be 10% larger than the region occupied
+    # by all loaded datasets
+    MA.setup_grid(50)
+    landscape_err_latent, landscape_err_3d, xaxis, yaxis = MA.scan_error()
+
+    fig, ax = plt.subplots()
+    c = ax.pcolormesh(xaxis, yaxis, landscape_err_latent)
+    plt.savefig('Error_grid.png')
 
 
 ## to visualise the GUI, execute the code above in a Jupyter notebook, then call:
 # from molearn.analysis import MolearnGUI
 # MolearnGUI(MA)
+# %%
