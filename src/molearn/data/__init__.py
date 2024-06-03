e2c55054c3756923fea6fb9fe758d4483da05a43
gwirn <71886945+gwirn@users.noreply.github.com>
2024-06-03 15:17:39 +0200

    added example for data preparation

--- src/molearn/data/__init__.py
+++ src/molearn/data/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Samuel C. Musson
+# Copyright (c) 2022 Samuel C. Musson, Gregor Wirnsberger
 #
 # Molearn is free software ;
 # you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ;
