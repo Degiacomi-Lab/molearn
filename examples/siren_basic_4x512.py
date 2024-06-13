d5b1b431c86bd2dbd05b55424772c3743c795a96
Asal Azar <yabiasal@gmail.com>
2024-06-13 10:19:41 +0100

    Train Siren models

--- examples/siren_basic_4x512.py
+++ examples/siren_basic_4x512.py
@@ -1,6 +1,8 @@
 #%%
 import sys, os
-sys.path.insert(0, os.path.join(os.path.abspath(os.pardir),'src'))
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+src_path = os.path.join(base_dir, 'src')
+sys.path.insert(0, src_path)
 from molearn.data import PDBData
 from molearn.trainers import OpenMM_Physics_Trainer
 from molearn.models.siren import AutoEncoder
@@ -11,7 +13,8 @@ if __name__ == '__main__':
 
     ##### Load Data #####
     data = PDBData()
-    data.import_pdb('data/aggregated_align_1-84.pdb')
+    data.import_pdb('/home/pghw87/Documents/molearn/Eng-asal/molearn/examples/data/MurD_closed_selection.pdb')
+    data.import_pdb('/home/pghw87/Documents/molearn/Eng-asal/molearn/examples/data/MurD_open_selection.pdb')
     #data.import_pdb('data/MurD_closed_selection.pdb')
     #data.import_pdb('data/MurD_open_selection.pdb')
     #data.import_pdb('/home3/pghw87/trajectories/MurD/MurD_closed.pdb')
@@ -27,7 +30,7 @@ if __name__ == '__main__':
     trainer.set_data(data, batch_size=8, validation_split=0.1, manual_seed = 25)
     trainer.prepare_physics(soft_NB = True)
     
-    trainer.set_autoencoder(AutoEncoder, mol = data.mol)
+    trainer.set_autoencoder(AutoEncoder, mol = data.mol, hidden_dimensions = [512, 512, 512, 512])
     #trainer.prepare_optimiser()
     trainer.optimiser = torch.optim.AdamW(trainer.autoencoder.parameters(), lr=1e-6, weight_decay=0.0001)
 
@@ -38,8 +41,8 @@ if __name__ == '__main__':
 
     runkwargs = dict(
         log_filename='log_file.dat',
-        log_folder='siren_checkpoints_nmr_alignall',
-        checkpoint_folder='siren_checkpoints_nmr_alignres',
+        log_folder='siren_checkpoints',
+        checkpoint_folder='siren_checkpoints',
         )
 
     best = 1e24
